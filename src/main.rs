0cc87f9c3b663aa6e71e195b89e1f8bcef197ef1
amor7seas <amor@siriux.ai>
2025-05-18 14:40:45 +0400

    feat(passthrough): done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use std::env;
+use std::process::{Command, exit};
+
 fn main() {
-    println!("Hello, world! ");
+    let args: Vec<String> = env::args().skip(1).collect();
+
+    let git_command = Command::new("git");
+
+    match args.get(0).map(String::as_str) {
+        Some("commit") => {
+            print!("hehe");
+        }
+        Some(_) => passthrough(git_command, args),
+        None => {
+            eprintln!("command not found");
+            exit(1);
+        }
+    }
+}
+
+fn passthrough(mut command: Command, args: Vec<String>) {
+    match command.args(args).output() {
+        Ok(output) => {
+            print!("{}", String::from_utf8_lossy(&output.stdout));
+
+            if !output.status.success() {
+                eprintln!("{}", String::from_utf8_lossy(&output.stderr));
+            }
+
+            exit(output.status.code().unwrap_or(1));
+        }
+        Err(err) => {
+            eprint!("gitlaw : Failed to run git: {}", err);
+            exit(1);
+        }
+    }
 }
